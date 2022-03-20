74cb9ea023815019a3bbe579bc86d09b4e3e9675
Ian Fay <ifay@my.wctc.edu>
2022-03-20 15:59:44 -0500

    1.05: Added different types of Ticket to Ticket.cs

--- Ticket.cs
+++ Ticket.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace midterm
 {
-    public class Ticket
+    public abstract class Ticket
     {
         public int ticketID { get; set; }
         public string summary { get; set; }
@@ -17,10 +17,47 @@ namespace midterm
 
         public string watching {get; set;}
 
-        public string Display()
+        public virtual string Display()
         {
             return ($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{watching}");
         }
 
     }
+
+    public class Bug: Ticket {
+        public string severity { get; set; }
+        public override string Display()
+        {
+            return ($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{watching}, {severity}");
+        }
+
+    }
+
+        public class Enhancement: Ticket {
+        
+        //Software, Cost, Reason, Estimate
+        public string software {get; set;}
+        public decimal cost {get; set;}
+        public string reason {get; set;}
+
+        public string estimate {get; set;}
+        public override string Display()
+        {
+            return ($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{watching}, {software}, {cost}, {reason}, {estimate}");
+        }
+
+    }
+
+        public class Task: Ticket {
+        
+        //ProjectName, DueDate
+        public string projectName {get; set;}
+        public string dueDate {get; set;}
+        public override string Display()
+        {
+            return ($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{watching}, {projectName}, {dueDate}");
+        }
+
+    }
+
 }
