6434e7609a848220796d3ee192da4c8b7e0d234d
Ian Fay <ifay@my.wctc.edu>
2022-03-20 12:51:38 -0500

    1.00: Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
